e75ffea982dc4034132b79ff1ca84d7aeeaec9d6
Karpde <karpde24@gmail.com>
2023-11-24 13:56:24 +0200

    adding first lab

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # first-repo-14-2023
 My first repo in group IP-14 in 2023
+adding first lab
---
+++ lab1.py
@@ -0,0 +1,27 @@
+music = "Neffex"
+print(music)
+
+score = 75
+
+if score >= 60:
+    print("Well done!Your score is:"+str(score))
+
+is_even = True
+is_odd = False
+
+if is_even == True:
+    print("number is even") 
+
+if is_odd != False:
+    print("number is odd")
+
+
+
+
+
+
+
+
+
+    #else:
+   # print("number is odd") 
